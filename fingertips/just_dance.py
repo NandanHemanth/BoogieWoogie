70a4ec726a0676357f149be0615f2ec577252379
Nandan H <theuniverseisflat101@gmail.com>
2024-09-29 02:06:28 -0400

    Ana dataset csv generated from video to calculate score

---
+++ fingertips/just_dance.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import time
+
+# initialising framework
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+pTime = 0
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    # print(results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x*w), int(lm.y*h)
+
+            # cv2.circle(img, (cx, cy), 3, (255,0,0), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    cv2.imshow("Image", img)
+
+    if  cv2.waitKey(2) & 0xFF == 27:
+        break
